0501616515358805729734c71653cabaa2d44c2c
sandroroux <alessandro@catalpa.io>
2020-04-06 18:53:41 -0400

    Fix translation plugin.

--- modules/riot-translation-plugin.js
+++ modules/riot-translation-plugin.js
@@ -2,19 +2,21 @@
 // install in your riot.install handler
 // Looks for all elements with a 'translate' attributen replaces content with gettext(content)
 
-import { getText } from 'translation'
+import { getText } from "translation";
 
-const ORIGINAL_TRANSLATION_ATTRIBUTE = 'canoe_translation_original';
+const ORIGINAL_TRANSLATION_ATTRIBUTE = "canoe_translation_original";
 
-export const installTranlsationPlugin = function(component) {
-    const originalOnUpdated = component.onUpdated || function() {};
+export const installTranslationPlugin = function (component) {
+    const originalOnUpdated = component.onUpdated || (() => {});
 
     component.onUpdated = function translateOnUpdated(...args) {
-        component.$$('[translate]').forEach(element => {
+        component.$$("[translate]").forEach((element) => {
             if (!element.hasAttribute(ORIGINAL_TRANSLATION_ATTRIBUTE)) {
                 element.setAttribute(ORIGINAL_TRANSLATION_ATTRIBUTE, element.innerText);
             }
             element.innerText = getText(element.getAttribute(ORIGINAL_TRANSLATION_ATTRIBUTE));
-        })
+        });
+
+        originalOnUpdated.apply(component, args);
     };
 };
