c04b46ffabe95a7549a4e72568ce29cee3322e30
sandroroux <alessandro@catalpa.io>
2020-04-08 12:35:20 -0400

    Browser support screen uses a Redux switch.

--- src/index.js
+++ src/index.js
@@ -3,21 +3,9 @@ import * as riot from "riot";
 
 import "./scss/olgeta.scss";
 import App from "RiotTags/App.riot.html";
-import ChangeBrowser from "RiotTags/screens/ChangeBrowser.riot.html";
 import { store } from "ReduxImpl/Store";
-import { getBrowserVersion } from "js/DjangoPushNotifications";
 
-const browser = getBrowserVersion(navigator.userAgent);
-const platform = navigator.platform;
-
-if (platform.indexOf("iPhone")!=-1 && browser.name == "Chrome") {
-    riot.register("changebrowser", ChangeBrowser);
-    window.addEventListener("load", () => {
-        riot.mount("changebrowser");
-    });
-} else {
-    riot.register("app", App);
-    window.addEventListener("load", () => {
-        riot.mount("app", { store });
-    });
-}
+riot.register("app", App);
+window.addEventListener("load", () => {
+    riot.mount("app", { store });
+});
