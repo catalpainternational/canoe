85ebde3ce874d44b36977266a5ca450e41b09b27
Kate Voss <katherineavoss@gmail.com>
2020-04-07 23:04:04 -0400

    Show message to change browser if on Chrome & iOS

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import * as riot from "riot";
 
 import "./scss/olgeta.scss";
 import App from "RiotTags/App.riot.html";
+import ChangeBrowser from "RiotTags/screens/ChangeBrowser.riot.html";
 import { store } from "ReduxImpl/Store";
+import { getBrowserVersion } from "js/DjangoPushNotifications";
 
-riot.register("app", App);
+const browser = getBrowserVersion(navigator.userAgent);
+const platform = navigator.platform;
 
-window.addEventListener("load", () => {
-    riot.mount("app", { store });
-});
+if (platform.indexOf("iPhone")!=-1 && browser.name == "Chrome") {
+    riot.register("changebrowser", ChangeBrowser);
+    window.addEventListener("load", () => {
+        riot.mount("changebrowser");
+    });
+} else {
+    riot.register("app", App);
+    window.addEventListener("load", () => {
+        riot.mount("app", { store });
+    });
+}
