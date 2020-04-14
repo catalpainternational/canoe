2ac5ef292ef3bc20a9cbda789746ebbe8ea4086e
PeteCoward <peter@catalpa.io>
2020-04-14 14:57:41 +0900

    Early out and double quotes

--- src/index.js
+++ src/index.js
@@ -7,26 +7,27 @@ let storeState = store.getState();
 
 store.subscribe( () => {
     const newStoreState = store.getState();
-    if(newStoreState.serviceWorker !== storeState.serviceWorker) {
+    if(newStoreState.serviceWorker === storeState.serviceWorker) {
+        return;
+    }
 
-        switch(newStoreState.serviceWorker) {
-            case "notsupported":
-                document.querySelector("#service-worker-loading").hidden = true;
-                document.querySelector("#service-worker-notsupported").hidden = false;
-                break;
-            case "installed":
-                // hide the loading splash
-                document.querySelector("#preapp-messages").hidden = true;
-                import(/* webpackChunkName: "app" */ "./app.js")
-                break;
-            case "updated":
-                // TODO should we reload? it might interrupt something
-                // should we prompt the user?
-                window.location.reload(true);
-                break;
-            default:
-                break;
-        }
+    switch(newStoreState.serviceWorker) {
+        case "notsupported":
+            document.querySelector("#service-worker-loading").hidden = true;
+            document.querySelector("#service-worker-notsupported").hidden = false;
+            break;
+        case "installed":
+            // hide the loading splash
+            document.querySelector("#preapp-messages").hidden = true;
+            import(/* webpackChunkName: "app" */ "./app.js")
+            break;
+        case "updated":
+            // TODO should we reload? it might interrupt something
+            // should we prompt the user?
+            window.location.reload(true);
+            break;
+        default:
+            break;
     }
 });
 
