6b8c0ffae73b78826ae8a9c0f433b8f322030cd6
PeteCoward <peter@catalpa.io>
2020-04-14 13:43:04 +0900

    kebab-case, better naming, double quotes

--- src/index.js
+++ src/index.js
@@ -10,16 +10,16 @@ store.subscribe( () => {
     if(newStoreState.serviceWorker !== storeState.serviceWorker) {
 
         switch(newStoreState.serviceWorker) {
-            case 'notsupported':
-                document.querySelector('#service_worker_loading').hidden = true;
-                document.querySelector('#service_worker_notsupported').hidden = false;
+            case "notsupported":
+                document.querySelector("#service-worker-loading").hidden = true;
+                document.querySelector("#service-worker-notsupported").hidden = false;
                 break;
-            case 'installed':
+            case "installed":
                 // hide the loading splash
-                document.querySelector('#canoe_loading').hidden = true;
+                document.querySelector("#preapp-messages").hidden = true;
                 import(/* webpackChunkName: "app" */ "./app.js")
                 break;
-            case 'updated':
+            case "updated":
                 // TODO should we reload? it might interrupt something
                 // should we prompt the user?
                 window.location.reload(true);
