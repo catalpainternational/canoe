269c868d706a55848ba7ac6b3dd86ffc59d282db
PeteCoward <peter@catalpa.io>
2020-05-01 14:45:14 +0900

    Production optimization for scss and js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,4 @@
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const cssnano = require('cssnano');
-
 module.exports =  {
     mode: 'production',
     devtool: false,
-    plugins: [
-        new OptimizeCSSAssetsPlugin({
-            cssProcessor: cssnano,
-            cssProcessorPluginOptions: {
-                preset: ['default', { discardComments: { removeAll: true } }],
-            },
-            canPrint: false
-        }),
-    ],
 }
