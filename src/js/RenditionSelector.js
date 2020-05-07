7b219bda2668001a60eaa109ebfdbff3c25d617a
PeteCoward <peter@catalpa.io>
2020-05-07 16:45:21 +0900

    Safari uses jpegs not unsupported jpegs

--- src/js/RenditionSelector.js
+++ src/js/RenditionSelector.js
@@ -2,20 +2,18 @@ import { BACKEND_BASE_URL } from "js/urls"
 import { getManifestFromStore } from "ReduxImpl/Store"
 
 const FILTER_SPEC = 'width-800|format-webp';
+const SAFARI_FILTER_SPEC = 'width-600|format-jpeg';
 
 export function getImagePath(id) {
     const images = getManifestFromStore().images;
-    return _getRenditionUrl(images[id]);
+    return _getRenditionUrl(images[id], true);
 }
 
 export function getImageUrls(images) {
-    return Object.values(images).map(_getRenditionUrlWithoutDomain);
+    return Object.values(images).map(_getRenditionUrl);
 }
 
-export const _getRenditionUrlWithoutDomain = (renditions) => {
-    return `/media/${renditions[FILTER_SPEC]}`;
-};
-
-function _getRenditionUrl(renditions) {
-    return `${BACKEND_BASE_URL}/media/${renditions[FILTER_SPEC]}`;
+function _getRenditionUrl(renditions, includeDomain=false) {
+    const filter_spec = navigator.userAgent.match(/Safari/g) ? SAFARI_FILTER_SPEC : FILTER_SPEC;
+    return `${BACKEND_BASE_URL}/media/${renditions[filter_spec]}`;
 }
