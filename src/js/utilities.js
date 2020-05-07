742e9ce5f53acba9998b98ddd062d00570b7c956
sandroroux <alessandro@catalpa.io>
2020-05-07 17:13:34 -0400

    This language switch uses old, non-redux APIs.

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -1,5 +1,9 @@
 import { getMostRecentCompletion } from "Actions/completion";
-import { getOrFetchManifest, getOrFetchWagtailPage } from "js/WagtailPagesAPI";
+import {
+    getOrFetchManifest,
+    getOrFetchWagtailPage,
+    getHomePagePathInCurrentLanguage,
+} from "js/WagtailPagesAPI";
 import { dispatchToastEvent } from "js/Events";
 
 export const alertAppIsOffline = () => {
@@ -25,7 +29,8 @@ export const getLastWorkedOnCourse = async () => {
     }
 
     const manifest = await getOrFetchManifest();
-    const homePage = await getOrFetchWagtailPage(manifest.home);
+    const homePagePath = getHomePagePathInCurrentLanguage(manifest);
+    const homePage = await getOrFetchWagtailPage(homePagePath);
     const courses = homePage.courses;
     const lastWorkedOnCourse = courses.find(
         (course) => course.data.slug === lastCompletion.courseSlug
