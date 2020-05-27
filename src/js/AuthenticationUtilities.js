67b666d7cdf5dc5c2bc297cea1b8fa52caeab888
Kate Voss <katherineavoss@gmail.com>
2020-05-27 15:49:44 -0400

    One way to maintain userLanguage after logout

--- src/js/AuthenticationUtilities.js
+++ src/js/AuthenticationUtilities.js
@@ -1,5 +1,6 @@
 import { BACKEND_BASE_URL } from "js/urls";
 import { dispatchLoggedOutEvent, dispatchSiteDownloadEvent } from "js/Events";
+import { getLanguage, changeLanguage } from "ReduxImpl/Store";
 
 const USERNAME_STORAGE_KEY = "username";
 const USER_ID_STORAGE_KEY = "userId";
@@ -81,6 +82,7 @@ export const login = async usernameAndPassword => {
 export const logout = async () => {
     deleteCookie(JWT_TOKEN_STORAGE_KEY);
     localStorage.clear();
+    changeLanguage(getLanguage());
     dispatchLoggedOutEvent();
 };
 
