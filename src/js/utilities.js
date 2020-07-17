c040fc54a8986c537fab5cbc709641360119e87d
sandroroux <alessandro@catalpa.io>
2020-07-17 14:09:32 -0400

    Debounces search. Search submits 250 milliseconds after the last keystroke.

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -55,3 +55,18 @@ export const getCourseAndLessonSlugs = (wagtailCoursePage) => {
         lessonSlugs,
     };
 };
+
+// From: https://levelup.gitconnected.com/debounce-in-javascript-improve-your-applications-performance-5b01855e086
+export const debounce = (func, waitInMilliseconds) => {
+    let timeout;
+
+    return function executedFunction(...args) {
+        const later = () => {
+            timeout = null;
+            func(...args);
+        };
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, waitInMilliseconds);
+    };
+};
