be8cfd1188ad5cdd78586f092da97cdd785f1b55
Alessandro Roux <alessandro@catalpa.io>
2020-08-10 17:22:34 -0400

    Fixes a JS bug that prevents users from browser-backing across translations.

--- src/js/Routing.js
+++ src/js/Routing.js
@@ -24,7 +24,7 @@ const getPreviewPageUrl = (queryString) => {
 
 const routeToTranslation = (translationPageId) => {
     const [, section, cardNumber] = parseURLHash();
-    const translatedPageUrl = `#${translationPageId}`;
+    let translatedPageUrl = `#${translationPageId}`;
     if (section) {
         translatedPageUrl += `/${section}/`;
     }
