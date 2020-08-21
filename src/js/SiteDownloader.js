d5238c4c4e2378b33d8116406d62d79b3cedf1c0
sandroroux <alessandro@catalpa.io>
2020-08-21 14:32:07 -0400

    Caches pages and media with Cache API.

--- src/js/SiteDownloader.js
+++ src/js/SiteDownloader.js
@@ -1,27 +1,17 @@
-import {
-    fetchPage,
-    fetchImage,
-    getOrFetchManifest,
-    getHomePathsInManifest,
-} from "js/WagtailPagesAPI";
-import { storeWagtailPage } from "ReduxImpl/Store";
+import { getOrFetchManifest, getHomePathsInManifest } from "js/WagtailPagesAPI";
 import { dispatchToastEvent } from "js/Events";
 import { leftDifference } from "js/SetMethods";
 import { getImagePaths } from "js/RenditionSelector";
+import { getAuthenticationToken } from "js/AuthenticationUtilities";
+import { storeWagtailPage } from "ReduxImpl/Store";
+import { BACKEND_BASE_URL } from "js/urls";
+
+const PAGES_CACHE = "pages-cache";
+const IMAGES_CACHE = "images-cache";
 
 const trimDomain = (urlWithDomain) => urlWithDomain.replace(/^.*\/\/[^\/]+/, "");
 
-const addCachedPageToRedux = async (cacheKeyRequest) => {
-    const pagesCache = await caches.open("pages-cache");
-    const cachedPage = await pagesCache.match(cacheKeyRequest);
-    if (!cachedPage) {
-        return;
-    }
-    const wagtailPage = await cachedPage.json();
-    storeWagtailPage(wagtailPage);
-};
-
-const getCachedUrlsAndDeleteCruft = async (cacheName, manifestsUrls) => {
+const getCachedPathsAndDeleteCruft = async (cacheName, manifestsUrls) => {
     const cache = await caches.open(cacheName);
     const cacheKeys = await cache.keys();
     const cachedUrls = new Set();
@@ -32,7 +22,6 @@ const getCachedUrlsAndDeleteCruft = async (cacheName, manifestsUrls) => {
 
         if (isCachedUrlInManifest) {
             cachedUrls.add(cachedUrl);
-            addCachedPageToRedux(request);
         } else {
             cache.delete(request);
         }
@@ -40,43 +29,63 @@ const getCachedUrlsAndDeleteCruft = async (cacheName, manifestsUrls) => {
     return cachedUrls;
 };
 
-export class SiteDownloader {
-    constructor() {
-        this.PAGES_CACHE = "pages-cache";
-        this.MEDIA_CACHE = "images-cache";
-    }
+const cacheAllUrls = async (cacheName, paths) => {
+    const cache = await window.caches.open(cacheName);
+    const token = getAuthenticationToken();
+    const pathArray = Array.from(paths);
 
+    return await cache.addAll(
+        pathArray.map(
+            (path) =>
+                new Request(`${BACKEND_BASE_URL}${path}`, {
+                    mode: "cors",
+                    headers: {
+                        "Content-Type": "text/json",
+                        Authorization: `JWT ${token}`,
+                    },
+                })
+        )
+    );
+};
+
+const addCachedPagesToRedux = async () => {
+    const pagesCache = await caches.open(PAGES_CACHE);
+    const cacheKeys = await pagesCache.keys();
+
+    for (const cacheKeyRequest of cacheKeys) {
+        const cachedPage = await pagesCache.match(cacheKeyRequest);
+        if (!cachedPage) {
+            return;
+        }
+        const wagtailPage = await cachedPage.json();
+        storeWagtailPage(wagtailPage);
+    }
+};
+
+export default class SiteDownloader {
     async requestTheSitesPagesAndImages() {
         const manifest = await getOrFetchManifest();
 
         const homePagePaths = getHomePathsInManifest(manifest);
-        const manifestsPageUrls = new Set([...homePagePaths, ...Object.values(manifest.pages)]);
-        const manifestsMediaUrls = new Set(getImagePaths(manifest.images));
+        const manifestsPagePaths = new Set([...homePagePaths, ...Object.values(manifest.pages)]);
+        const manifestsImagesPaths = new Set(getImagePaths(manifest.images));
 
-        const cachedPageUrls = await getCachedUrlsAndDeleteCruft(
-            this.PAGES_CACHE,
-            manifestsPageUrls
-        );
-        const cachedMediaUrls = await getCachedUrlsAndDeleteCruft(
-            this.MEDIA_CACHE,
-            manifestsMediaUrls
+        const cachedPagePaths = await getCachedPathsAndDeleteCruft(PAGES_CACHE, manifestsPagePaths);
+        const cachedImagePaths = await getCachedPathsAndDeleteCruft(
+            IMAGES_CACHE,
+            manifestsImagesPaths
         );
 
-        const pagesToFetch = leftDifference(manifestsPageUrls, cachedPageUrls);
-        const imagesToFetch = leftDifference(manifestsMediaUrls, cachedMediaUrls);
+        const pagesToFetch = leftDifference(manifestsPagePaths, cachedPagePaths);
+        const imagesToFetch = leftDifference(manifestsImagesPaths, cachedImagePaths);
 
         if (pagesToFetch.size > 0 || imagesToFetch.size > 0) {
             dispatchToastEvent(gettext("Site is downloading."));
         }
 
-        for (const pagePath of pagesToFetch) {
-            const wagtailPage = await fetchPage(pagePath);
-            storeWagtailPage(wagtailPage);
-        }
-
-        for (const imagePath of imagesToFetch) {
-            await fetchImage(imagePath);
-        }
+        await cacheAllUrls(PAGES_CACHE, pagesToFetch);
+        await cacheAllUrls(IMAGES_CACHE, imagesToFetch);
+        await addCachedPagesToRedux();
 
         if (pagesToFetch.size > 0 || imagesToFetch.size > 0) {
             dispatchToastEvent(gettext("Site download is complete!"));
