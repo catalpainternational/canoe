cbd3a1299caf0902ae311df7be72015cc4fdded4
sandroroux <alessandro@catalpa.io>
2020-09-10 12:21:04 -0400

    Tests all CoursePage methods.

--- test/course.test.js
+++ test/course.test.js
@@ -2,10 +2,16 @@ import courseWithExam from "./pageData/courseWithExam";
 import courseWithoutExam from "./pageData/courseWithoutExam";
 
 import CoursePage from "js/CoursePage";
+import ExamGrader from "js/ExamGrader";
 
-import { getFinishedLessonSlugs } from "Actions/completion";
+import { getFinishedLessonSlugs, getLatestCompletionInCourse } from "Actions/completion";
+import { hasUserTriedExam as hasTriedExam, getExamHighScore as getHighScore } from "Actions/exam";
+import { _getOrFetchWagtailPageById } from "js/WagtailPagesAPI";
 
 jest.mock("Actions/completion");
+jest.mock("js/ExamGrader");
+jest.mock("Actions/exam");
+jest.mock("js/WagtailPagesAPI");
 
 describe("Tests CoursePage's properties.", () => {
     test.each([courseWithExam, courseWithoutExam])(
@@ -99,8 +105,87 @@ describe("Tests CoursePage's methods.", () => {
     ])(
         "CoursePage.isExamFinished() reflects whether course exam is finished.",
         (courseJSON, expected) => {
-            // const cp = new CoursePage(courseJSON);
-            // expect(cp.isExamFinished()).toBe(expected);
+            ExamGrader.isExamFinished.mockReturnValue(expected);
+
+            const cp = new CoursePage(courseJSON);
+            expect(cp.isExamFinished()).toBe(expected);
+        }
+    );
+
+    test.each([
+        [courseWithExam, true],
+        [courseWithoutExam, false],
+    ])(
+        "CoursePage.hasUserTriedExam() reflects whether course exam was tried.",
+        (courseJSON, expected) => {
+            hasTriedExam.mockReturnValue(expected);
+
+            const cp = new CoursePage(courseJSON);
+            expect(cp.hasUserTriedExam()).toBe(expected);
+        }
+    );
+
+    test.each([
+        [courseWithExam, 99],
+        [courseWithoutExam, 0],
+    ])("CoursePage.getExamHighScore() gives exam's high score.", (courseJSON, expected) => {
+        getHighScore.mockReturnValue(expected);
+
+        const cp = new CoursePage(courseJSON);
+        expect(cp.getExamHighScore()).toBe(expected);
+    });
+
+    test.each`
+        courseJSON           | finishedLessons                             | expected
+        ${courseWithExam}    | ${["slug-1", "slug-2", "exam"]}             | ${true}
+        ${courseWithoutExam} | ${["slug-1", "slug-2", "slug-3", "slug-4"]} | ${true}
+        ${courseWithExam}    | ${["slug-1", "slug-2"]}                     | ${false}
+        ${courseWithoutExam} | ${["slug-1", "slug-2", "slug-3"]}           | ${false}
+    `(
+        "With $courseJSON.data.lessons_count total lessons and $finishedLessons.length finished lessons, CoursePage.isFinished() returns $expected.",
+        ({ courseJSON, finishedLessons, expected }) => {
+            getFinishedLessonSlugs.mockReturnValue(finishedLessons);
+
+            const cp = new CoursePage(courseJSON);
+            expect(cp.isFinished()).toBe(expected);
+        }
+    );
+
+    test.each`
+        courseJSON           | expected
+        ${courseWithExam}    | ${{ courseSlug: "course-1", lessonSlug: "lesson-1", sectionSlug: "test", completionDate: new Date() }}
+        ${courseWithoutExam} | ${{ courseSlug: "course-1", lessonSlug: "lesson-1", sectionSlug: "test", completionDate: new Date() }}
+    `("CoursePage.getLatestCompletion() returns completion data.", ({ courseJSON, expected }) => {
+        getLatestCompletionInCourse.mockReturnValue(expected);
+
+        const cp = new CoursePage(courseJSON);
+        expect(cp.getLatestCompletion()).toBe(expected);
+    });
+
+    test.each`
+        courseJSON           | expected
+        ${courseWithExam}    | ${true}
+        ${courseWithoutExam} | ${false}
+    `("CoursePage.isVisibleToGuests() returns $expected.", ({ courseJSON, expected }) => {
+        const cp = new CoursePage(courseJSON);
+        expect(cp.isVisibleToGuests()).toBe(expected);
+    });
+
+    test.each`
+        courseJSON           | lessonJSON
+        ${courseWithExam}    | ${{ id: 50, data: { slug: "test-lesson" } }}
+        ${courseWithoutExam} | ${{ id: 50, data: { slug: "test-lesson" } }}
+    `(
+        "CoursePage.getFullLessonObjects() returns correct Array of LessonPages.",
+        async ({ courseJSON, lessonJSON }) => {
+            _getOrFetchWagtailPageById.mockReturnValue(lessonJSON);
+
+            const cp = new CoursePage(courseJSON);
+            const lessons = await cp.getFullLessonObjects();
+            for (const lesson of lessons) {
+                expect(lesson.id).toBe(50);
+                expect(lesson.slug).toBe("test-lesson");
+            }
         }
     );
 });
