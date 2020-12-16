73498966886fb7e7257a00e3bd4119e1034291b8
md8n <lee@md8n.com>
2020-12-16 12:46:15 +0900

    Test cache lock

--- src/ts/AppelflapConnect.ts
+++ src/ts/AppelflapConnect.ts
@@ -8,66 +8,66 @@ import {
 } from "ts/Types/CacheTypes";
 
 export class AppelflapConnect {
-    #localHostURI = "http://127.0.0.1";
+    readonly localHostURI = "http://127.0.0.1";
 
     #portNo = -1;
-    #metaApi = "meta";
-    #cacheApi = "api/ingeblikt";
-    #actionApi = "do";
+    readonly metaApi = "meta";
+    readonly cacheApi = "api/ingeblikt";
+    readonly actionApi = "do";
 
-    #lock = "insertion-lock";
-    #publications = "publications";
-    #subscriptions = "subscriptions";
-    #status = "status";
-    #reboot = "reboot";
+    readonly insLock = "insertion-lock";
+    readonly publications = "publications";
+    readonly subscriptions = "subscriptions";
+    readonly status = "status";
+    readonly reboot = "reboot";
 
     private _commands = {
         getMetaStatus: {
-            commandPath: `${this.#metaApi}/${this.#status}`,
+            commandPath: `${this.metaApi}/${this.status}`,
             method: "GET",
         },
         setLock: {
-            commandPath: `${this.#cacheApi}/${this.#lock}`,
+            commandPath: `${this.cacheApi}/${this.insLock}`,
             method: "PUT",
         },
         releaseLock: {
-            commandPath: `${this.#cacheApi}/${this.#lock}`,
+            commandPath: `${this.cacheApi}/${this.insLock}`,
             method: "DELETE",
         },
         getCacheStatus: {
-            commandPath: `${this.#cacheApi}/${this.#status}`,
+            commandPath: `${this.cacheApi}/${this.status}`,
             method: "GET",
         },
         doReboot: {
-            commandPath: `${this.#actionApi}/${this.#reboot}`,
+            commandPath: `${this.actionApi}/${this.reboot}`,
             method: "POST",
         },
         getPublications: {
-            commandPath: `${this.#cacheApi}/${this.#publications}`,
+            commandPath: `${this.cacheApi}/${this.publications}`,
             method: "GET",
         },
         savePublication: {
-            commandPath: `${this.#cacheApi}/${this.#publications}`,
+            commandPath: `${this.cacheApi}/${this.publications}`,
             method: "PUT",
         },
         deletePublication: {
-            commandPath: `${this.#cacheApi}/${this.#publications}`,
+            commandPath: `${this.cacheApi}/${this.publications}`,
             method: "DELETE",
         },
         getSubscriptions: {
-            commandPath: `${this.#cacheApi}/${this.#subscriptions}`,
+            commandPath: `${this.cacheApi}/${this.subscriptions}`,
             method: "GET",
         },
         subscribe: {
-            commandPath: `${this.#cacheApi}/${this.#subscriptions}`,
+            commandPath: `${this.cacheApi}/${this.subscriptions}`,
             method: "PUT",
         },
         unsubscribe: {
-            commandPath: `${this.#cacheApi}/${this.#subscriptions}`,
+            commandPath: `${this.cacheApi}/${this.subscriptions}`,
             method: "DELETE",
         },
         bulkSubscribe: {
-            commandPath: `${this.#cacheApi}/${this.#subscriptions}`,
+            commandPath: `${this.cacheApi}/${this.subscriptions}`,
             method: "POST",
         },
     } as {
@@ -75,7 +75,7 @@ export class AppelflapConnect {
     };
 
     /** Get the port number that Appelflap is using or return -1 */
-    public getPortNo(): number {
+    get portNo(): number {
         // Appelflap knows the port number (which is randomly picked at launch); and it needs to let the web context know what the port number is.
         // It could be learned via a call to a contentscript (WebExtension), but that won't work in a ServiceWorker. Hence, we stuff it in the language
         // tags, which are available inside a ServiceWorker context.
@@ -109,21 +109,20 @@ export class AppelflapConnect {
         commandPath: string,
         commandInit?: RequestInit
     ): Promise<Response> => {
-        const requestInfo = `${
-            this.#localHostURI
-        }:${this.getPortNo()}/${commandPath}`;
+        const requestInfo = `${this.localHostURI}:${this.portNo}/${commandPath}`;
 
         /** When we finally have authorization via Appelflap, we'll change this */
         const weHazAuthorization = false;
 
-        const requestInit =
-            !weHazAuthorization ||
-            !commandInit ||
-            JSON.stringify(commandInit) == '{"method":"GET"}'
-                ? undefined
-                : commandInit
-                ? commandInit
-                : ({} as RequestInit);
+        const requestInitRequired =
+            weHazAuthorization ||
+            (commandInit && JSON.stringify(commandInit) !== '{"method":"GET"}');
+
+        const requestInit = !requestInitRequired
+            ? undefined
+            : commandInit
+            ? commandInit
+            : ({} as RequestInit);
 
         if (weHazAuthorization) {
             // Add the authorization header
@@ -140,7 +139,13 @@ export class AppelflapConnect {
         const response = await this.appelflapFetch(commandPath, commandInit);
 
         if (!response.ok) {
-            return "Nope, that's not OK";
+            switch (response.status) {
+                case 401:
+                case 404:
+                    return Promise.reject(response.statusText);
+            }
+
+            return Promise.reject("Nope, that's not OK");
         }
 
         switch (returnType) {
--- src/ts/tests/AppelflapConnect.test.ts
+++ src/ts/tests/AppelflapConnect.test.ts
@@ -1,5 +1,6 @@
 import test from "ava";
 import fetchMock from "fetch-mock";
+import { Response } from "node-fetch";
 
 import { buildFakeNavigator } from "./fakeNavigator";
 
@@ -10,7 +11,7 @@ test("getPortNo (Appelflap test)", (t: any) => {
     global["navigator"] = buildFakeNavigator(testPort);
     const afc = new AppelflapConnect();
     t.is(
-        afc.getPortNo(),
+        afc.portNo,
         testPort,
         "navigator has encoded portNo - implies Appelflap"
     );
@@ -18,19 +19,20 @@ test("getPortNo (Appelflap test)", (t: any) => {
     global["navigator"] = buildFakeNavigator();
     const nfc = new AppelflapConnect();
     t.is(
-        nfc.getPortNo(),
+        nfc.portNo,
         -1,
         "navigator does not have encoded portNo - implies no Appelflap"
     );
 });
 
-test("getMetaStatus", (t: any) => {
+/** meta status is not expected to be used by the Canoe-Appelflap cache API */
+test("getMetaStatus", async (t: any) => {
     const testPort = 9090;
     global["navigator"] = buildFakeNavigator(testPort);
 
-    const localHostURI = "http://127.0.0.1";
-    const metaApi = "meta";
-    const status = "status";
+    const afc = new AppelflapConnect();
+    const testUri = `${afc.localHostURI}:${testPort}/${afc.metaApi}/${afc.status}`;
+
     const state = {
         diskused: 554058,
         diskfree: 69899911168,
@@ -47,12 +49,36 @@ test("getMetaStatus", (t: any) => {
             },
         ],
     };
-    fetchMock.get(`${localHostURI}:${testPort}/${metaApi}/${status}`, state);
+    fetchMock.get(testUri, state);
+
+    const result = await afc.getMetaStatus();
+    fetchMock.reset();
+    t.deepEqual(result, state);
+});
+
+test("Cache: lock", async (t: any) => {
+    const testPort = 9090;
+    global["navigator"] = buildFakeNavigator(testPort);
 
     const afc = new AppelflapConnect();
-
-    return afc.getMetaStatus().then((result: any) => {
-        fetchMock.reset();
-        t.deepEqual(result, state);
+    const testUri = `${afc.localHostURI}:${testPort}/${afc.cacheApi}/${afc.insLock}`;
+    const successResponse = new Response("ok", {
+        status: 200,
+        statusText: "Ok",
     });
+    const authFailureResponse = new Response(undefined, {
+        status: 401,
+        statusText: "Not Authorized",
+    });
+
+    fetchMock.put(testUri, successResponse);
+    const successResult = await afc.lock();
+    t.is(successResult, "ok");
+
+    fetchMock.put(testUri, authFailureResponse);
+    await afc.lock().catch((reason) => {
+        t.is(reason, "Not Authorized");
+    });
+
+    fetchMock.reset();
 });
