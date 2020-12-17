c72778c436784c835423758fd97bdb9ff360f09e
md8n <lee@md8n.com>
2020-12-17 10:50:39 +0900

    test subscriptions

--- src/ts/tests/AppelflapConnect.test.ts
+++ src/ts/tests/AppelflapConnect.test.ts
@@ -4,8 +4,13 @@ import { Response } from "node-fetch";
 
 import { buildFakeNavigator } from "./fakeNavigator";
 
-import { AppelflapConnect } from "../AppelflapConnect";
-import { TPublication } from "../Types/CacheTypes";
+import { AppelflapConnect } from "ts/AppelflapConnect";
+import {
+    TPublication,
+    TPublicationTarget,
+    TSubscription,
+    TSubscriptions,
+} from "ts/Types/CacheTypes";
 
 test.before((t: any) => {
     t.context["testPort"] = 9090;
@@ -18,9 +23,34 @@ test.beforeEach((t: any) => {
         status: 200,
         statusText: "Ok",
     });
+
     t.context["authFailureResponse"] = new Response(undefined, {
         status: 401,
         statusText: "Not Authorized",
+    });
+
+    // When the version supplied is invalid
+    t.context["badRequestResponse"] = new Response(undefined, {
+        status: 400,
+        statusText: "Bad Request",
+    });
+
+    // when Appelflap can't find the designated cache
+    t.context["notFoundResponse"] = new Response(undefined, {
+        status: 404,
+        statusText: "Not Found",
+    });
+
+    // when a packing action is already in progress for this cache
+    t.context["conflictResponse"] = new Response(undefined, {
+        status: 409,
+        statusText: "Conflict",
+    });
+
+    // when Appelflap is too busy to comply (perhaps it's packing up some other cache)
+    t.context["serviceUnavailableResponse"] = new Response(undefined, {
+        status: 503,
+        statusText: "Service Unavailable",
     });
 });
 
@@ -187,7 +217,12 @@ test("Cache: getPublications", async (t: any) => {
 test("Cache: publish", async (t: any) => {
     const afc = t.context.afc as AppelflapConnect;
     const successResponse = t.context.successResponse as Response;
+    const badRequestResponse = t.context.badRequestResponse as Response;
     const authFailureResponse = t.context.authFailureResponse as Response;
+    const notFoundResponse = t.context.notFoundResponse as Response;
+    const conflictResponse = t.context.conflictResponse as Response;
+    const serviceUnavailableResponse = t.context
+        .serviceUnavailableResponse as Response;
 
     const webOrigin = "some-web-origin";
     const cacheName = "some-cache-name";
@@ -198,30 +233,6 @@ test("Cache: publish", async (t: any) => {
         cacheName: cacheName,
         version: version,
     };
-
-    // When the version supplied is invalid
-    const badRequestResponse = new Response(undefined, {
-        status: 400,
-        statusText: "Bad Request",
-    });
-
-    // when Appelflap can't find the designated cache
-    const notFoundResponse = new Response(undefined, {
-        status: 404,
-        statusText: "Not Found",
-    });
-
-    // when a packing action is already in progress for this cache
-    const conflictResponse = new Response(undefined, {
-        status: 409,
-        statusText: "Conflict",
-    });
-
-    // when Appelflap is too busy to comply (perhaps it's packing up some other cache)
-    const serviceUnavailableResponse = new Response(undefined, {
-        status: 503,
-        statusText: "Service Unavailable",
-    });
 
     fetchMock.put(testUri, successResponse);
     const successResult = await afc.publish(publication);
@@ -261,6 +272,8 @@ test("Cache: unpublish", async (t: any) => {
     const afc = t.context.afc as AppelflapConnect;
     const successResponse = t.context.successResponse as Response;
     const authFailureResponse = t.context.authFailureResponse as Response;
+    const notFoundResponse = t.context.notFoundResponse as Response;
+    const conflictResponse = t.context.conflictResponse as Response;
 
     const webOrigin = "some-web-origin";
     const cacheName = "some-cache-name";
@@ -271,18 +284,6 @@ test("Cache: unpublish", async (t: any) => {
         cacheName: cacheName,
         version: version,
     };
-
-    // when Appelflap can't find the designated cache
-    const notFoundResponse = new Response(undefined, {
-        status: 404,
-        statusText: "Not Found",
-    });
-
-    // when a packing action is already in progress for this cache
-    const conflictResponse = new Response(undefined, {
-        status: 409,
-        statusText: "Conflict",
-    });
 
     fetchMock.delete(testUri, successResponse);
     const successResult = await afc.unpublish(publication);
@@ -306,10 +307,146 @@ test("Cache: unpublish", async (t: any) => {
     fetchMock.reset();
 });
 
-test.todo("Cache: getSubscriptions");
+test("Cache: getSubscriptions", async (t: any) => {
+    const afc = t.context.afc as AppelflapConnect;
+    const authFailureResponse = t.context.authFailureResponse as Response;
 
-test.todo("Cache: subscribe");
+    const testUri = `${afc.localHostURI}:${t.context.testPort}/${afc.cacheApi}/${afc.subscriptions}`;
+    const testResponse = {
+        "some-web-origin": {
+            "some-cache-name": { "Version-Min": 10, "Version-Max": 9000 },
+        },
+    };
 
-test.todo("Cache: unsubscribe");
+    const successResponse = new Response(JSON.stringify(testResponse), {
+        status: 200,
+        statusText: "Ok",
+        headers: { "Content-Type": "application/json" },
+    });
 
-test.todo("Cache: bulkSubscribe");
+    fetchMock.get(testUri, successResponse);
+    const successResult = await afc.getSubscriptions();
+    t.deepEqual(successResult, testResponse);
+
+    fetchMock.get(testUri, authFailureResponse, { overwriteRoutes: true });
+    await afc.getSubscriptions().catch((reason) => {
+        t.is(reason, "Not Authorized");
+    });
+
+    fetchMock.reset();
+});
+
+test("Cache: subscribe", async (t: any) => {
+    const afc = t.context.afc as AppelflapConnect;
+    const successResponse = t.context.successResponse as Response;
+    const authFailureResponse = t.context.authFailureResponse as Response;
+    const conflictResponse = t.context.conflictResponse as Response;
+
+    const webOrigin = "some-web-origin";
+    const cacheName = "some-cache-name";
+    const versionMin = 10;
+    const versionMax = 9000;
+    const testUri = `${afc.localHostURI}:${t.context.testPort}/${afc.cacheApi}/${afc.subscriptions}/${webOrigin}/${cacheName}`;
+    const subscription: TSubscription = {
+        webOrigin: webOrigin,
+        cacheName: cacheName,
+        "Version-Min": versionMin,
+        "Version-Max": versionMax,
+    };
+
+    fetchMock.put(testUri, successResponse);
+    const successResult = await afc.subscribe(subscription);
+    t.is(successResult, "ok");
+
+    fetchMock.put(testUri, authFailureResponse, { overwriteRoutes: true });
+    await afc.subscribe(subscription).catch((reason) => {
+        t.is(reason, "Not Authorized");
+    });
+
+    fetchMock.put(testUri, conflictResponse, { overwriteRoutes: true });
+    await afc.subscribe(subscription).catch((reason) => {
+        t.is(reason, "Conflict");
+    });
+
+    fetchMock.reset();
+});
+
+test("Cache: unsubscribe", async (t: any) => {
+    const afc = t.context.afc as AppelflapConnect;
+    const successResponse = t.context.successResponse as Response;
+    const authFailureResponse = t.context.authFailureResponse as Response;
+    const notFoundResponse = t.context.notFoundResponse as Response;
+    const conflictResponse = t.context.conflictResponse as Response;
+
+    const webOrigin = "some-web-origin";
+    const cacheName = "some-cache-name";
+    const testUri = `${afc.localHostURI}:${t.context.testPort}/${afc.cacheApi}/${afc.subscriptions}/${webOrigin}/${cacheName}`;
+    const subscription: TPublicationTarget = {
+        webOrigin: webOrigin,
+        cacheName: cacheName,
+    };
+
+    fetchMock.delete(testUri, successResponse);
+    const successResult = await afc.unsubscribe(subscription);
+    t.is(successResult, "ok");
+
+    fetchMock.delete(testUri, authFailureResponse, { overwriteRoutes: true });
+    await afc.unsubscribe(subscription).catch((reason) => {
+        t.is(reason, "Not Authorized");
+    });
+
+    fetchMock.delete(testUri, notFoundResponse, { overwriteRoutes: true });
+    await afc.unsubscribe(subscription).catch((reason) => {
+        t.is(reason, "Not Found");
+    });
+
+    fetchMock.delete(testUri, conflictResponse, { overwriteRoutes: true });
+    await afc.unsubscribe(subscription).catch((reason) => {
+        t.is(reason, "Conflict");
+    });
+
+    fetchMock.reset();
+});
+
+test("Cache: bulkSubscribe", async (t: any) => {
+    const afc = t.context.afc as AppelflapConnect;
+    const successResponse = t.context.successResponse as Response;
+    const badRequestResponse = t.context.badRequestResponse as Response;
+    const authFailureResponse = t.context.authFailureResponse as Response;
+
+    const testUri = `${afc.localHostURI}:${t.context.testPort}/${afc.cacheApi}/${afc.subscriptions}`;
+    const subscriptions: TSubscriptions = {
+        "some-web-origin": {
+            "some-cache-name": {
+                "Version-Min": 10,
+                "Version-Max": 9000,
+            },
+            "some-other-cache-name": {
+                "Version-Min": 10,
+                "Version-Max": 9000,
+            },
+        },
+        "some-other-web-origin": {
+            "yet-another-cache-name": {
+                "Version-Min": 10,
+                "Version-Max": 9000,
+            },
+        },
+    };
+
+    fetchMock.post(testUri, successResponse);
+    const successResult = await afc.bulkSubscribe(subscriptions);
+    t.is(successResult, "ok");
+
+    fetchMock.post(testUri, badRequestResponse, { overwriteRoutes: true });
+    await afc.bulkSubscribe(subscriptions).catch((reason) => {
+        t.is(reason, "Bad Request");
+    });
+
+    fetchMock.post(testUri, authFailureResponse, { overwriteRoutes: true });
+    await afc.bulkSubscribe(subscriptions).catch((reason) => {
+        t.is(reason, "Not Authorized");
+    });
+
+    fetchMock.reset();
+});
