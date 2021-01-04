b0c158c8e6a70487cc8e82e2c349d9d21d647e42
md8n <lee@md8n.com>
2021-01-04 15:10:21 +0900

    Try moving appelflap tests into app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import * as riot from "riot";
 import { installReduxPlugin } from "ReduxImpl/RiotReduxPlugin";
 import { installTranslationPlugin } from "riot/RiotTranslationPlugin";
 
+import { getPlatform } from "js/PlatformDetection";
+import { AppelflapConnect } from "ts/AppelflapConnect";
+
 import App from "RiotTags/App.riot.html";
 import "./scss/canoe.scss";
 
@@ -12,4 +15,35 @@ riot.install(function (component) {
 });
 
 riot.register("app", App);
-riot.mount("app");
+
+const inAppelflap = () => {
+    const { browser, inAppelflap, inPWAMode } = getPlatform();
+    return inAppelflap;
+};
+
+/** Tell Appelflap that Canoe is 'locked'
+ * and should not be rebooted */
+const lockCanoe = async () => {
+    const appelflapConnect = new AppelflapConnect();
+    return await appelflapConnect.lock();
+};
+
+if (inAppelflap()) {
+    lockCanoe()
+        .then((lockResult) => {
+            if (lockResult !== "ok") {
+                // The lock didn't succeed, the app is probably still usable.
+                // We don't yet understand what this means for the user and the app.
+                // Should we show a small warning?
+            }
+
+            riot.mount("app");
+        })
+        .catch((_) => {
+            /* Do nothing */
+        });
+} else {
+    riot.mount("app");
+}
+
+lockCanoe();
