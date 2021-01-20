2a156b5ca476bb84e396738c2439cec235d981e3
md8n <lee@md8n.com>
2021-01-20 15:45:21 +0900

    warnings

--- src/ts/Redux/Interface.ts
+++ src/ts/Redux/Interface.ts
@@ -2,6 +2,7 @@ import { store } from "ReduxImpl/Store";
 
 import { addManifestV2 as addManifestV2Action } from "ts/Redux/Ducks/SiteV2";
 
-export const storeManifestV2 = (manifestV2: any) => {
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const storeManifestV2 = (manifestV2: any): void => {
     store.dispatch(addManifestV2Action(manifestV2));
 };
