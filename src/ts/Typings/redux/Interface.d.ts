1978dcc64994ccc4ec8f80cad96079aa435f2536
md8n <lee@md8n.com>
2021-01-20 15:35:03 +0900

    removed more redundancies, unused typings

--- src/ts/Typings/redux/Interface.d.ts
+++
@@ -1,7 +0,0 @@
-// js/redux/Interface
-
-/** Note that the module name here MUST match how it's used in the .ts files */
-declare module "ReduxImpl/Interface" {
-    export function getManifestFromStore(): any;
-    export function storeManifest(manifest: any): void;
-}
