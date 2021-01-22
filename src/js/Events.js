3779146a0228f53ef20189db3d0920cf9ddb3bd5
PeteCoward <peter@catalpa.io>
2021-01-23 08:16:42 +0900

    Remove site download event

--- src/js/Events.js
+++ src/js/Events.js
@@ -1,18 +1,12 @@
 // Custom events go here (prepend the app name to the event).
 const APP_NAME = "canoe";
 
-export const ON_REQUEST_SITE_DOWNLOAD = `${APP_NAME}.app.download`;
 export const ON_LOG_IN = `${APP_NAME}.auth.loggedIn`;
 export const ON_LOG_OUT = `${APP_NAME}.auth.loggedOut`;
 export const ON_ADD_TO_HOME_SCREEN = `${APP_NAME}.sw.addToHomeScreen`;
 export const ON_COMPLETION_CHANGE = `${APP_NAME}.completions.change`;
 export const ON_ACTION_CHANGE = `${APP_NAME}.actions.change`;
 export const ON_ANSWERED_TEST_QUESTION = `${APP_NAME}.tests.answeredQuestion`;
-
-export const dispatchSiteDownloadEvent = () => {
-    const siteDownloadEvent = new Event(ON_REQUEST_SITE_DOWNLOAD);
-    dispatchEvent(siteDownloadEvent);
-};
 
 export const dispatchLoggedInEvent = () => {
     const loggedInEvent = new Event(ON_LOG_IN);
