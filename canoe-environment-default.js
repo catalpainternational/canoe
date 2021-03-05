76a38dcd46ac6e9aec938276cc01f318ac1fe8af
Meggan Turner <meggan@meggans-air.home>
2021-03-05 16:45:59 +1100

    readd canoe-environment-default.js file which was accidentally deleted

---
+++ canoe-environment-default.js
@@ -0,0 +1,16 @@
+// environment configuration is things related to where
+// the code is running, on a dev machine or a deployment
+
+module.exports = {
+    GA_TAG: false,
+    API_BASE_URL: "http://localhost:8000",
+    SKIP_SW: false,
+    DONT_SHOW_COMPLETIONS_AFTER: "2020-2-15",
+    APPLICATION_SERVER_KEY: "",
+    WEBPACK_CONFIG: {
+        devServer: {
+            port: 8080,
+        }
+    },
+    GUEST_USERNAME: "Guest"
+};
