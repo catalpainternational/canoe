efe33b943d43aa415cdcd201b0e3677fc6fe646c
Kate Voss <kate@catalpa.io>
2021-03-10 11:32:22 -0500

    Show card image, make cards distinct from other cards

--- src/ts/Implementations/Specific/LearningActivityRoot.ts
+++ src/ts/Implementations/Specific/LearningActivityRoot.ts
@@ -4,4 +4,8 @@ export default class LearningActivityRoot extends Page {
     get resources(): Page[] {
         return this.childPages;
     }
+
+    get description(): string {
+        return this.data.description;
+    }
 }
