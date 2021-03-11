19d5fd1b3ad5ebee044ad31967631fe4780aab24
PeteCoward <peter@catalpa.io>
2021-03-11 15:24:35 +0900

    Resources list improvements

--- src/ts/Implementations/Specific/ResourcesRoot.ts
+++ src/ts/Implementations/Specific/ResourcesRoot.ts
@@ -1,7 +1,21 @@
 import { Page } from "../Page";
 
 export default class ResourcesRoot extends Page {
+    #all_tags!: Set<string>;
+
     get resources(): Page[] {
         return this.childPages;
+    }
+
+    get all_tags(): Set<string> {
+        if (this.#all_tags === undefined) {
+            this.#all_tags = new Set(
+                this.resources
+                    .map((r: any) => r.tags)
+                    .flat()
+                    .map((tag: string) => tag.toLowerCase())
+            );
+        }
+        return this.#all_tags;
     }
 }
