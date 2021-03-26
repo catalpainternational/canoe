93c70106b95403f48926dec9eecb1e2b8e9f1756
Meggan <megganeturner@gmail.com>
2021-03-26 11:54:33 +1100

    467-question card with images (#543)
    * add card image to questions if they have one
    
    * add image to exam questions, fix logic for examCards in .ts (still TODO for id attribute), remove bottom menu for exam pages
    
    * remove TODO comment, id now coming through from api

--- src/ts/Implementations/Specific/Course.ts
+++ src/ts/Implementations/Specific/Course.ts
@@ -17,10 +17,10 @@ export default class Course extends Page {
         return this.data.data?.has_exam;
     }
     get examCards(): any[] {
-        const examCards = this.data.exam.map((card: any) => {
-            const { id, type, value } = card;
-            const { question, answers } = value;
-            return { id, type, question, answers };
+        const examCards = this.data.exam_cards.map((card: any) => {
+            const type = card.tag;
+            const { id, question, answers, tag, image } = card;
+            return { id, type, question, answers, tag, image };
         });
         return examCards;
     }
