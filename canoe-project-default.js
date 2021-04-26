12181c498cec023a775c4b5787ac179fda449f74
PeteCoward <peter@catalpa.io>
2021-04-26 11:46:09 +0900

    Update icons and manifest (#637)

--- canoe-project-default.js
+++ canoe-project-default.js
@@ -15,7 +15,8 @@ module.exports = {
     SITE_NAME: "Canoe",
     SITE_SHORT_NAME: "Canoe",
     SITE_DESCRIPTION: "An online education platform",
-    FAVICON_PATH: "./src/favicon.svg",
+    FAVICON_PATH: "./src/favicon.ico",
+    ICON_PATH: "./src/icon.png",
     // If true REQUIRE_LOGIN will always redirect any user who does not have a login to the login page
     // unless they have a manifest cache in place already
     REQUIRE_LOGIN: false,
