5b8065faf8dbf806577d6f9fe914397296cf2c7f
sandroroux <alessandro@catalpa.io>
2021-05-24 11:55:20 -0400

    An env variable can set Canoe's language.

--- src/js/redux/Store.js
+++ src/js/redux/Store.js
@@ -6,10 +6,20 @@ export const LANGUAGE_STORAGE_KEY = "userLanguage";
 
 const getInitialLanguage = () => {
     const storedLanguage = localStorage.getItem(LANGUAGE_STORAGE_KEY);
+    const envLanguage = process.env.CANOE_DEFAULT_LANGUAGE;
 
     if (!!storedLanguage) {
         return storedLanguage;
-    } else if (navigator.language.includes("en")) {
+    }
+    
+    // Allow an env-set default language.
+    if (["tet", "en"].includes(envLanguage)) {
+        localStorage.setItem(LANGUAGE_STORAGE_KEY, envLanguage);
+        return envLanguage;
+    }
+
+    // Set language with the browser setting.
+    if (navigator.language.includes("en")) {
         localStorage.setItem(LANGUAGE_STORAGE_KEY, "en");
         return "en";
     } else {
