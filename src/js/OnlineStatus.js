b4d4b6d4320ab947a74aae1d0815f5e739c900fb
Meggan Turner <megganeturner@gmail.com>
2021-06-02 12:21:17 +1000

    update offline logic to refer to `interface.js`

--- src/js/OnlineStatus.js
+++ src/js/OnlineStatus.js
@@ -1,6 +1,11 @@
 import { setOnline, setOffline } from "ReduxImpl/Interface";
 
 export function initialiseOnlineStatus(window) {
+    if (navigator.onLine) {
+        setOnline();
+    } else {
+        setOffline();
+    }
     // subscribe to online and offline events to inform the store
     window.addEventListener("online", setOnline);
     window.addEventListener("offline", setOffline);
