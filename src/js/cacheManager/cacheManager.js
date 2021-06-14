f7b47cb5fa24524c1f251cf11e0d4dbce4e6859d
md8n <lee@md8n.com>
2021-06-14 15:08:25 +0900

    Couple of quick clean ups (avoid confusion) (#746)

--- src/js/cacheManager/cacheManager.js
+++ src/js/cacheManager/cacheManager.js
@@ -1,37 +1,6 @@
 /// CacheManager
 // a set of simple functions to easily wrap and expose cache operations
 
-import { BACKEND_BASE_URL } from "js/urls";
-
-// requires ignoreVary:true but I am not sure why!
-const CACHE_OPTIONS = { ignoreVary: true };
-
-export async function isItemCached(itemUrl) {
-    // check if an url is cached ( in any Cache )
-    const match = await caches.match(itemUrl, CACHE_OPTIONS);
-    return match ? true : false;
-}
-
-export async function listCache(cacheName) {
-    // get an array of all the urls in a named Cache
-    const cache = await caches.open(cacheName);
-    const keys = await cache.keys();
-    return keys.map(k => `${BACKEND_BASE_URL}${k.url}`);
-}
-
-export async function deleteItemFromCache(itemUrl, cacheName) {
-    // deletes an item from a named Cache
-    const cache = await caches.open(cacheName);
-    return await cache.delete(itemUrl, CACHE_OPTIONS);
-}
-
-export async function addToCache(itemUrl, cacheName) {
-    // adds an item to a named Cache
-    // works for now becasue our media is served with no authentication requirements
-    const cache = await caches.open(cacheName);
-    return await cache.add(itemUrl);
-}
-
 export async function getCachesInfo() {
     // iterates caches returning an array of
     // { name, size, count } for each cache
