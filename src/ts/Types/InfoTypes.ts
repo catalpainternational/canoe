1c4cb1c2891fce8da87b8d3af3f0625e3cc63ea9
boerwastaken <cat@gavagai.eu>
2021-07-06 11:22:06 +0200

    reflect upstream field naming consistentialization

--- src/ts/Types/InfoTypes.ts
+++ src/ts/Types/InfoTypes.ts
@@ -1,9 +1,9 @@
 /** A set of values that identify this user, as a peer for sharing */
 export type TPeerProperties = {
-    ID: number;
+    id: number;
     /** A 4 character representation of the @see ID, that has been derived from the @see palette */
-    friendly_ID: string;
-    /** This is the 'palette' of characters from which the @see friendly_ID has been formed */
+    friendly_id: string;
+    /** This is the 'palette' of characters from which the @see friendly_id has been formed */
     palette?: string;
 };
 
