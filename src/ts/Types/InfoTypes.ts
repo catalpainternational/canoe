58b95ce6b8982ec893abcbb6a74063ac708a3fbd
boerwastaken <cat@gavagai.eu>
2021-07-06 11:22:31 +0200

    reflect upstream spelling correction

--- src/ts/Types/InfoTypes.ts
+++ src/ts/Types/InfoTypes.ts
@@ -14,14 +14,13 @@ export type TInfoWiFi = {
     ssid: string;
     /**
      * IP Address correctly formatted
-     * @remarks Note the spelling mistake in `ipadress`
      */
-    ipadress: string;
+    ipaddress: string;
     /**
      * IP Address as a simple number
-     * @remarks Note the spelling mistake in `ipadress_raw`
      */
-    ipadress_raw: number;
+    ipaddress_raw: number;
+    /** This is expressed as a number in the range 0-100 (inclusive) */
     strength: number;
 };
 
